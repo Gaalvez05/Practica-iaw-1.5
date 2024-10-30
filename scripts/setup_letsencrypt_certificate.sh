977ddbedf7b0073202d8649efc906ca4fa89b564
Antonio Jesus <agalrod813@g.educaand.es>
2024-10-30 08:23:10 +0000

    CAMBIOS

--- scripts/setup_letsencrypt_certificate.sh
+++ scripts/setup_letsencrypt_certificate.sh
@@ -16,3 +16,8 @@ apt remove certbot -y
 #Instalamos el cliente de Certbot con snapd.
 snap install --classic certbot
 
+# Creamos una alias para el comando certbot.
+ln -fs /snap/bin/certbot /usr/bin/certbot
+
+#Solicitamos un certificado a Let's Encrypt
+certbot --apache -m $LE_EMAIL --agree-tos --no-eff-email -d $LE_DOMAIN --non-interactive
